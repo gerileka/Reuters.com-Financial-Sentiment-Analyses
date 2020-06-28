194706db0f1b28229b23789a5fd4c72c13be8980
gerileka <gerileka@hotmail.com>
2020-06-28 20:11:26 +0200

    link function

---
+++ src/Link_function.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def get_one_link(url):
+    import requests
+    from bs4 import BeautifulSoup
+    
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    news_link = []
+    column_href = soup.find('div', class_='news-headline-list')
+    for link in column_href.find_all('a'):
+        news_link.append('https://www.reuters.com'+link.get('href'))
+    news_link = list(set(news_link))
+    
+    return news_link
+
+
+def get_links(pages):
+    
+    import itertools
+    
+    news_url = []
+    for p in range(1,pages):
+        url = 'https://www.reuters.com/news/archive/bank-news?view=page&page='+str(p)+'&pageSize=10'
+        print(url)
+        news_url.append(get_one_link(url))
+    news_url = list(set(list(itertools.chain(*news_url))))
+    
+    return news_url
+
+
+# ### _Test_ 
+
+# In[5]:
+
+
+# url= 'https://www.reuters.com/news/archive/bank-news?view=page&page=1&pageSize=10'
+# get_links(url)
+
+
+# In[ ]:
+
+
+
+
